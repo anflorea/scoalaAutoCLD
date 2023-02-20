5d04c8072336745211c895d71a8d555d22b1cbd0
Tudor Linca <linca.tudor@gmail.com>
2023-02-20 14:59:18 +0200

    Added poster image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import background from './background.jpg';
 import './App.css';
+import background from './images/background.jpg';
+import posterImage from './images/CLD_poster_A3.jpg';
 
 const redirectLink = 'https://www.facebook.com/scoalaAutoCLD';
 
@@ -20,12 +21,13 @@ function App() {
       }}
     >
       <header className="App-header">
-        <h1>In curand</h1>
+        <h1>În curând</h1>
+        <img src={posterImage} />
         <p className="description">
-          Site-ul nostru este in lucru si va fi publicat in cel mai scurt timp.
-          Pana atunci ne puteti vizita pagina de facebook. Multumim pentru
-          intelegere.!
+          Site-ul nostru este în lucru și va fi publicat în cel mai scurt timp.
+          Pană atunci ne puteți vizita pagina de Facebook.
         </p>
+        <p className="description"> Vă mulțumim pentru înțelegere!</p>
         <button
           onClick={() => {
             redirect(redirectLink);
