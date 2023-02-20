23894b60980ca63d2a6740253ad3a56191ce415a
Tudor Linca <linca.tudor@gmail.com>
2023-02-20 14:35:49 +0200

    Added text and redirect button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import background from './background.jpg';
 import './App.css';
 
+const redirectLink = 'https://www.facebook.com/scoalaAutoCLD';
+
+const redirect = (url) => {
+  if (url !== '' && url.slice(0, 4) === 'http') {
+    window.location.href = url;
+  }
+
+  var g = 'https://www.google.com';
+};
+
 function App() {
   return (
     <div
@@ -10,9 +20,20 @@ function App() {
       }}
     >
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>In curand</h1>
+        <p className="description">
+          Site-ul nostru este in lucru si va fi publicat in cel mai scurt timp.
+          Pana atunci ne puteti vizita pagina de facebook. Multumim pentru
+          intelegere.!
         </p>
+        <button
+          onClick={() => {
+            redirect(redirectLink);
+          }}
+          className="button"
+        >
+          Facebook
+        </button>
       </header>
     </div>
   );
