a990078630e040654a352990fd692cee8243a2cc
Tudor Linca <linca.tudor@gmail.com>
2023-03-20 17:56:30 +0200

    Removed unused images in each modal

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -26,7 +26,7 @@ const Curriculum = () => {
       projectInfo:
         'Autovehiculul a carui masă totala maxima autorizata nu depaseste 3.500 Kg si al carui numar de locuri pe scaun, in afara conducatorului, nu este mai mare de 8.',
       thumbImage: 'images/projects/CatB.png',
-      sliderImages: ['images/projects/CatB.png', 'images/projects/CatB.png'],
+      // sliderImages: ['images/projects/CatB.png', 'images/projects/CatB.png'],
       categories: [filters.B.replace(/\s+/g, '')],
     },
     {
@@ -34,10 +34,10 @@ const Curriculum = () => {
       projectInfo:
         'Autovehiculul a carui masa totala maxima autorizata nu depaseste 3.500 Kg si al carui numar de locuri pe scaun, in afara conducatorului, nu este mai mare de 8, iar transmisia este cu cutie automată. Ulterior promovării examenului, deținătorul permisului având dreptul de a conduce doar autovehicule cu cutie automată.',
       thumbImage: 'images/projects/CutieAutomata.jpeg',
-      sliderImages: [
-        'images/projects/CutieAutomata.jpeg',
-        'images/projects/CutieAutomata.jpeg',
-      ],
+      // sliderImages: [
+      //   'images/projects/CutieAutomata.jpeg',
+      //   'images/projects/CutieAutomata.jpeg',
+      // ],
       categories: [filters.B.replace(/\s+/g, '')],
     },
     {
@@ -45,10 +45,10 @@ const Curriculum = () => {
       projectInfo:
         'Cvadriciclurile și triciclurile, autovehiculele cu trei sau patru roți, care au masă proprie de peste 400 kg, însă nu mai mare de 550 kg. Aceste mașini sunt echipate cu motor cu ardere internă, a carui capacitate cilindrică este mai mare de 45 cmc sau cu orice alt motor care oferăo putere echivalentă ori cu viteză prin construcție mai mare de 50 km/h.',
       thumbImage: 'images/projects/CatB1.jpeg',
-      sliderImages: [
-        'images/projects/CatB1.jpeg',
-        'images/projects/CatB1.jpeg',
-      ],
+      // sliderImages: [
+      //   'images/projects/CatB1.jpeg',
+      //   'images/projects/CatB1.jpeg',
+      // ],
       categories: [filters.B.replace(/\s+/g, '')],
     },
     {
@@ -56,10 +56,10 @@ const Curriculum = () => {
       projectInfo:
         'Autovehiculul a carui masă totala maxima autorizata este mai mare de 3500 Kg si care este proiectat pentru transportul a maximum 8 pasageri.',
       thumbImage: 'images/projects/SoferiProfesionisti.png',
-      sliderImages: [
-        'images/projects/SoferiProfesionisti.png',
-        'images/projects/SoferiProfesionisti.png',
-      ],
+      // sliderImages: [
+      //   'images/projects/SoferiProfesionisti.png',
+      //   'images/projects/SoferiProfesionisti.png',
+      // ],
       categories: [filters.PROFESSIONAL.replace(/\s+/g, '')],
     },
     {
@@ -67,10 +67,10 @@ const Curriculum = () => {
       projectInfo:
         'Ansamblu format dintr-un autovehicul tragator din categoria C si o remorcă a carei masa totala maxima autorizata este mai mare de 750 Kg.',
       thumbImage: 'images/projects/SoferiProfesionisti2.jpeg',
-      sliderImages: [
-        'images/projects/SoferiProfesionisti2.jpeg',
-        'images/projects/SoferiProfesionisti2.jpeg',
-      ],
+      // sliderImages: [
+      //   'images/projects/SoferiProfesionisti2.jpeg',
+      //   'images/projects/SoferiProfesionisti2.jpeg',
+      // ],
       categories: [filters.PROFESSIONAL.replace(/\s+/g, '')],
     },
   ];
