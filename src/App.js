3ab2252e576412658b49931e68f944be2bd95d31
Tudor Linca <linca.tudor@gmail.com>
2023-05-06 03:31:55 +0300

    Enabled Register Section

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ function App() {
         classicHeader: true,
         darkTheme: false,
         testimonialsSection: true,
-        registerSection: false,
+        registerSection: true,
       }}
     >
       <AppContent />
